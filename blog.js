f8b30d7f2ad67461daae848eec38c8f843eaab95
tarikjamil <tarikjamill@gmail.com>
2025-01-19 12:01:42 +0100

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,28 +1,48 @@
-// ------------------ accordion ------------------ //
+$(document).ready(function () {
+  // ------------------ accordion ------------------ //
 
-$(".filters--accordion-trigger").on("click", function () {
-  // Close other accordions when opening new one
-  if (!$(this).hasClass("open")) {
-    $(".filters--accordion-trigger.open").click();
-  }
-  // Save the sibling of the toggle we clicked on
-  let sibling = $(this).siblings(".filters--accordion-list");
-  let animationDuration = 500;
+  $(".filters--accordion-trigger").on("click", function (event) {
+    // Prevent closing the parent when clicking on inner dropdown
+    if ($(event.target).closest(".filters--accordion-list").length) {
+      return;
+    }
 
-  if ($(this).hasClass("open")) {
-    // Close the content div if already open
-    sibling.animate({ height: "0px" }, animationDuration);
-  } else {
-    // Open the content div if already closed
-    sibling.css("height", "auto");
-    let autoHeight = sibling.height();
-    sibling.css("height", "0px");
-    sibling.animate({ height: autoHeight }, animationDuration, () => {
+    // Close other accordions when opening new one
+    if (!$(this).hasClass("open")) {
+      $(".filters--accordion-trigger.open").not(this).click();
+    }
+
+    // Save the sibling of the toggle we clicked on
+    let sibling = $(this).siblings(".filters--accordion-list");
+    let animationDuration = 500;
+
+    if ($(this).hasClass("open")) {
+      // Close the content div if already open
+      sibling.animate({ height: "0px" }, animationDuration);
+    } else {
+      // Open the content div if already closed
       sibling.css("height", "auto");
+      let autoHeight = sibling.height();
+      sibling.css("height", "0px");
+      sibling.animate({ height: autoHeight }, animationDuration, () => {
+        sibling.css("height", "auto");
+      });
+    }
+    // Open and close the toggle div
+    $(this).toggleClass("open");
+  });
 
-      // Scroll the page to the accordion, leaving 200 pixels from the top
-    });
-  }
-  // Open and close the toggle div
-  $(this).toggleClass("open");
+  // ------------------ close dropdown on outside click ------------------ //
+  $(document).on("click", function (event) {
+    // Check if the click is outside any open dropdown
+    if (!$(event.target).closest(".filters--accordion").length) {
+      $(".filters--accordion-trigger.open").each(function () {
+        // Close the dropdown
+        $(this).removeClass("open");
+        $(this)
+          .siblings(".filters--accordion-list")
+          .animate({ height: "0px" }, 500);
+      });
+    }
+  });
 });
